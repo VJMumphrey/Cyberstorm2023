e0bec22a485f15982865add477b0eb5d6c2bb232
Caleb Matherne (PC) <ctm042@latech.edu>
2023-03-30 00:22:02 -0500

    updated covert client

--- Covert Chat/client.py
+++ Covert Chat/client.py
@@ -2,19 +2,20 @@ from socket import *
 from sys import stdout
 from time import time
 
-ZERO = 0.025
-ONE = 0.1
+ZERO = 0.1
+ONE = 0.025
 threshhold = (ZERO + ONE)/2
 
 
 iplist = ["localhost","138.47.99.64"]
-ip = iplist[0]
+ip = iplist[1]
 port = 31337
 
 # AF_INET is a network stack
-# Type of communication (TCP protocol)
+# SOCK_STREAM is the type of communication (TCP protocol)
 s = socket(AF_INET, SOCK_STREAM)
 
+print("[connect to the chat server]")
 s.connect((ip, port))
 
 data = s.recv(4096).decode()
@@ -22,26 +23,31 @@ data = s.recv(4096).decode()
 msg = ""
 covert_bin = ""
 
+print("...")
 while (data.rstrip("\n") != "EOF"):
     stdout.write(data)
-    msg += data
     stdout.flush()
+    msg += data
     t0 = time()
     data = s.recv(4096).decode()
     t1 = time()
 
     delta = round(t1 - t0, 3)
-    stdout.write(f" {delta}\n")
     if delta < threshhold:
         covert_bin += "0"
     else:
         covert_bin += "1"
 
+stdout.write("...\n")
+
+print("[disconnect from the chat server]")
+s.close()
+
 covert = ""
 for i in range (0,len(covert_bin), 8):
     covert += chr(int(covert_bin[i:i+8], 2))
 
-s.close()
 covert = covert[:len(covert)-3]
-stdout.write(msg)
-stdout.write(covert)
+stdout.write(f"Covert message: {covert}\n")
+stdout.flush()
+
