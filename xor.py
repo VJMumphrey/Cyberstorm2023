a9b7a28b6a269927fb1651169de1c9f0aed3e4ea
Caleb Matherne (PC) <ctm042@latech.edu>
2023-05-11 18:18:00 -0500

    changed input method to require key and cipher file names as arguments

--- xor.py
+++ xor.py
@@ -2,15 +2,14 @@ import os
 import sys
 import base64
 
-# SETTINGS
-KEY_FILE = "key2"
-
-
-
-
 # main
 if __name__ == '__main__':
-    # method for xor'ing two file together
-    key = bytearray(open(KEY_FILE, "rb").read())
-    cipher = bytearray(sys.stdin.buffer.read())
-    sys.stdout.buffer.write(bytearray(a ^ b for a, b in zip(cipher, key)))
+    if(len(sys.argv) == 3):
+        KEY_FILE = sys.argv[1].replace(".py","")
+        CIPHER_FILE = sys.argv[2].replace(".py","")
+        # method for xor'ing two file together
+        key = bytearray(open(KEY_FILE, "rb").read())
+        cipher = bytearray(open(CIPHER_FILE, "rb").read())
+        sys.stdout.buffer.write(bytearray(a ^ b for a, b in zip(cipher, key)))
+    else:
+        print("Incorrect usage.\n$ python xor.py <key> <cipher>")
